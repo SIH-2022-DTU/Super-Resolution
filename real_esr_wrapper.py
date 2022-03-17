b5ce57a2f0bc67112a97447cb5f64c243ee451b5
Bhuvan-21 <sachdeva.bhuvan21@gmail.com>
2022-03-17 16:33:12 +0530

    added face option

--- real_esr_wrapper.py
+++ real_esr_wrapper.py
@@ -3,6 +3,7 @@ from basicsr.archs.rrdbnet_arch import RRDBNet
 from dotenv import load_dotenv
 from realesrgan import RealESRGANer
 import numpy as np
+from gfpgan import GFPGANer
 
 load_dotenv()
 class ISR():
@@ -12,7 +13,7 @@ class ISR():
         self.netscale = 4
         self.upsampler = RealESRGANer(scale=self.netscale, model_path=self.model_path, model=self.model)
         self.upsampler_tiled = RealESRGANer(scale=self.netscale, model_path=self.model_path, model=self.model,tile =tile )
-    
+
     
     def define_enhancer(self,image_shape = (64,64,3)):
         """
@@ -24,7 +25,7 @@ class ISR():
         else :
             self.enhancer = self.upsampler
 
-    def get_super_resolution(self,image,upscale_factor:int = 4):
+    def get_super_resolution(self,image,enhance_face:bool = False,upscale_factor:int = 4):
         """
         Args:
             image (numpy.array) : image of shape (h,w,c)
@@ -32,6 +33,16 @@ class ISR():
             upscale_factor (int): default -> 4 -> image wil be upscale to sh x sw x c from h x w x c where s is the upscale factor
         """
         output = None
+        if enhance_face:
+            face_enhancer = GFPGANer(
+            model_path='https://github.com/TencentARC/GFPGAN/releases/download/v1.3.0/GFPGANv1.3.pth',
+            upscale=upscale_factor,
+            arch='clean',
+            channel_multiplier=2,
+            bg_upsampler=self.upsampler)
+            _, _, output = face_enhancer.enhance(image, has_aligned=False, only_center_face=False, paste_back=True)
+            return output
+        
         if(image.shape[0]*image.shape[1] > 128*128):
             output = self.upsampler_tiled.enhance(image, outscale=upscale_factor)
         else:
